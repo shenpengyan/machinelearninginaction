d55611e1f9a302f9baff622f71d8f47f0ba0af1d
shenpengyan <shenpengyan@xiaomi.com>
2018-07-30 21:33:37 +0800

    add ch03

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -1,5 +1,20 @@
+# coding: utf-8
 from math import log
+import operator
+import treePlotter
+"""
+使用ID3 算法
+"""
+# 当最终叶子节点标签不唯一时，采用多数表决法
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys() : classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse= True)
+    return sortedClassCount[0][0]
 
+# 计算给定数据集的香农熵
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
@@ -14,7 +29,7 @@ def calcShannonEnt(dataSet):
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
 
-
+# 创建数据集
 def createDataSet() :
     dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
@@ -24,4 +39,93 @@ myDat, labels = createDataSet()
 
 rst = calcShannonEnt(myDat)
 
-print(rst)
+# 划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+# 选择最好的数据集划分方式
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0;
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain) :
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+print chooseBestFeatureToSplit(myDat)
+
+# 创建树
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    # 只剩一个
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 不能再分数据集了
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+myTree = createTree(myDat, labels[:])
+
+
+# 分类
+def classify(inputTree, featlabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featlabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featlabels, testVec)
+            else :
+                classLabel = secondDict[key]
+    return classLabel
+import pickle
+
+# 存储树
+def storeTree(inputTree, filename):
+    fw = open(filename, 'w')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+# 载入树
+def grabTree(filename):
+    fr = open(filename)
+    return pickle.load(fr)
+
+fr = open('lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+lenseTree = createTree(lenses, lensesLabels)
+print(lenseTree)
+
+treePlotter.createPlot(lenseTree)
+
